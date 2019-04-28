3102af34056a26a6224927f0a0eda8e8c54772e2
Konstantin Krivopustov <krivopustov@haulmont.com>
2019-04-28 21:01:03 +0400

    Added UI module with stock Vaadin 8

--- starters/starter-standard/src/main/java/io/jmix/autoconfiguration/JmixStandardAutoconfiguration.java
+++ starters/starter-standard/src/main/java/io/jmix/autoconfiguration/JmixStandardAutoconfiguration.java
@@ -2,10 +2,11 @@ package io.jmix.autoconfiguration;
 
 import io.jmix.core.JmixCoreConfiguration;
 import io.jmix.security.JmixSecurityConfiguration;
+import io.jmix.ui.JmixUiConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({JmixCoreConfiguration.class, JmixSecurityConfiguration.class})
+@Import({JmixCoreConfiguration.class, JmixSecurityConfiguration.class, JmixUiConfiguration.class})
 public class JmixStandardAutoconfiguration {
 }
