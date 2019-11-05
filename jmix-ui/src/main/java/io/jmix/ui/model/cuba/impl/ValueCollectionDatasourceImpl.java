6340058886e1ae62ec4e05ba003a34e3f1835487
Vlad Minaev <minaev@haulmont.com>
2019-11-05 13:09:13 +0400

    Copy legacy datasources from CUBA

---
+++ jmix-ui/src/main/java/io/jmix/ui/model/cuba/impl/ValueCollectionDatasourceImpl.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2019 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.jmix.ui.model.cuba.impl;
+
+import com.haulmont.chile.core.datatypes.Datatype;
+import com.haulmont.chile.core.model.MetaClass;
+import com.haulmont.cuba.core.app.keyvalue.KeyValueMetaClass;
+import com.haulmont.cuba.core.entity.KeyValueEntity;
+import com.haulmont.cuba.core.global.View;
+import com.haulmont.cuba.gui.data.CollectionDatasource;
+import com.haulmont.cuba.gui.data.DataSupplier;
+import com.haulmont.cuba.gui.data.DsContext;
+import com.haulmont.cuba.gui.logging.UIPerformanceLogger;
+import org.perf4j.StopWatch;
+import org.perf4j.slf4j.Slf4JStopWatch;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nullable;
+import java.util.Map;
+
+/**
+ * {@link CollectionDatasource} that supports {@link KeyValueEntity}.
+ */
+public class ValueCollectionDatasourceImpl
+        extends CollectionDatasourceImpl<KeyValueEntity, Object>
+        implements ValueDatasource {
+
+    protected final ValueDatasourceDelegate delegate;
+
+    public ValueCollectionDatasourceImpl() {
+        delegate = new ValueDatasourceDelegate(this);
+    }
+
+    @Override
+    public void setup(DsContext dsContext, DataSupplier dataSupplier, String id, MetaClass metaClass, @Nullable View view) {
+        this.id = id;
+        this.dsContext = dsContext;
+        this.dataSupplier = dataSupplier;
+        this.metaClass = new KeyValueMetaClass();
+    }
+
+    @Override
+    public ValueCollectionDatasourceImpl setIdName(String name) {
+        delegate.setIdName(name);
+        return this;
+    }
+
+    public ValueCollectionDatasourceImpl addProperty(String name) {
+        delegate.addProperty(name);
+        return this;
+    }
+
+    public ValueCollectionDatasourceImpl addProperty(String name, Class aClass) {
+        delegate.addProperty(name, aClass);
+        return this;
+    }
+
+    public ValueCollectionDatasourceImpl addProperty(String name, Datatype datatype) {
+        delegate.addProperty(name, datatype);
+        return this;
+    }
+
+    @Override
+    protected void loadData(Map<String, Object> params) {
+        String tag = getLoggingTag("VDS");
+        StopWatch sw = new Slf4JStopWatch(tag, LoggerFactory.getLogger(UIPerformanceLogger.class));
+
+        delegate.loadData(params);
+
+        sw.stop();
+    }
+
+    @Override
+    public void includeItem(KeyValueEntity item) {
+        super.includeItem(item);
+        item.setMetaClass(metaClass);
+    }
+
+    @Override
+    public void addItem(KeyValueEntity item) {
+        super.addItem(item);
+        item.setMetaClass(metaClass);
+    }
+
+    public void setStoreName(String storeName) {
+        this.delegate.setStoreName(storeName);
+    }
+}
---
+++ jmix-ui/src/main/java/io/jmix/ui/model/cuba/impl/WeakItemPropertyChangeListener.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2019 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.jmix.ui.model.cuba.impl;
+
+import com.haulmont.cuba.gui.data.Datasource;
+import com.haulmont.cuba.gui.data.Datasource.ItemPropertyChangeListener;
+
+import java.lang.ref.WeakReference;
+
+public class WeakItemPropertyChangeListener implements ItemPropertyChangeListener {
+
+    private final Datasource datasource;
+    private final WeakReference<ItemPropertyChangeListener> reference;
+
+    public WeakItemPropertyChangeListener(Datasource datasource,
+                                          ItemPropertyChangeListener itemPropertyChangeListener) {
+        this.datasource = datasource;
+        this.reference = new WeakReference<>(itemPropertyChangeListener);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void itemPropertyChanged(Datasource.ItemPropertyChangeEvent e) {
+        ItemPropertyChangeListener itemPropertyChangeListener = reference.get();
+        if (itemPropertyChangeListener != null) {
+            itemPropertyChangeListener.itemPropertyChanged(e);
+        } else {
+            datasource.removeItemPropertyChangeListener(this);
+        }
+    }
+}
