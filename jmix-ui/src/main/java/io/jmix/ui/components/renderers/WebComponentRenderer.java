a1ae4d4fdd0aa1f6874b0b8f15acaa819bbec5ed
Vlad Minaev <minaev@haulmont.com>
2019-11-20 14:42:21 +0400

    Copy ui component renderers from CUBA

---
+++ jmix-ui/src/main/java/io/jmix/ui/components/renderers/WebComponentRenderer.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2019 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.jmix.ui.components.renderers;
+
+import com.haulmont.cuba.core.entity.Entity;
+import com.haulmont.cuba.gui.components.DataGrid;
+import com.haulmont.cuba.web.gui.components.WebAbstractDataGrid.AbstractRenderer;
+import com.vaadin.data.ValueProvider;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.renderers.ComponentRenderer;
+import com.vaadin.ui.renderers.Renderer;
+
+/**
+ * A renderer for UI components.
+ */
+public class WebComponentRenderer<T extends Entity> extends AbstractRenderer<T, com.vaadin.ui.Component>
+        implements DataGrid.ComponentRenderer {
+
+    @Override
+    protected Renderer<Component> createImplementation() {
+        return new ComponentRenderer();
+    }
+
+    @Override
+    public ValueProvider<Component, Component> getPresentationValueProvider() {
+        return (ValueProvider<Component, Component>) value ->
+                value.unwrap(com.vaadin.ui.Component.class);
+    }
+}
