be00be28c2b7e518c80d065ae5d7761a1e115a8d
Vlad Minaev <minaev@haulmont.com>
2019-11-23 11:59:23 +0400

    Copy picker actions from CUBA

---
+++ jmix-ui/src/main/java/io/jmix/ui/actions/picker/ClearAction.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright 2019 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.jmix.ui.actions.picker;
+
+import com.haulmont.chile.core.model.MetaProperty;
+import com.haulmont.cuba.client.ClientConfig;
+import com.haulmont.cuba.core.entity.Entity;
+import com.haulmont.cuba.core.global.Configuration;
+import com.haulmont.cuba.core.global.Messages;
+import com.haulmont.cuba.gui.components.ActionType;
+import com.haulmont.cuba.gui.components.Component;
+import com.haulmont.cuba.gui.components.PickerField;
+import com.haulmont.cuba.gui.components.actions.BaseAction;
+import com.haulmont.cuba.gui.components.data.ValueSource;
+import com.haulmont.cuba.gui.components.data.meta.EntityValueSource;
+import com.haulmont.cuba.gui.icons.CubaIcon;
+import com.haulmont.cuba.gui.icons.Icons;
+import com.haulmont.cuba.gui.meta.StudioAction;
+import com.haulmont.cuba.gui.model.DataContext;
+import com.haulmont.cuba.gui.screen.FrameOwner;
+import com.haulmont.cuba.gui.screen.UiControllerUtils;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.Nullable;
+import javax.inject.Inject;
+
+/**
+ * Standard picker field action for clearing the field value.
+ * <p>
+ * Should be defined for {@code PickerField} or its subclass in a screen XML descriptor.
+ */
+@StudioAction(category = "Picker Actions", description = "Clears the picker field value")
+@ActionType(ClearAction.ID)
+public class ClearAction extends BaseAction implements PickerField.PickerFieldAction, InitializingBean {
+
+    public static final String ID = "picker_clear";
+
+    protected PickerField pickerField;
+    protected Icons icons;
+    protected Messages messages;
+    protected Configuration configuration;
+
+    protected boolean editable = true;
+
+    public ClearAction() {
+        super(ID);
+    }
+
+    public ClearAction(String id) {
+        super(id);
+    }
+
+    @Inject
+    protected void setConfiguration(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
+    @Inject
+    protected void setMessages(Messages messages) {
+        this.messages = messages;
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        setShortcut(configuration.getConfig(ClientConfig.class).getPickerClearShortcut());
+        setDescription(messages.getMainMessage("pickerField.action.clear.tooltip")
+                + " (" + getShortcutCombination().format() + ")");
+    }
+
+    @Override
+    public void setPickerField(@Nullable PickerField pickerField) {
+        this.pickerField = pickerField;
+    }
+
+    @Override
+    public void editableChanged(PickerField pickerField, boolean editable) {
+        setEditable(editable);
+
+        if (editable) {
+            setIcon(icons.get(CubaIcon.PICKERFIELD_CLEAR));
+        } else {
+            setIcon(icons.get(CubaIcon.PICKERFIELD_CLEAR_READONLY));
+        }
+    }
+
+    @Override
+    public boolean isEditable() {
+        return editable;
+    }
+
+    protected void setEditable(boolean editable) {
+        boolean oldValue = this.editable;
+        if (oldValue != editable) {
+            this.editable = editable;
+            firePropertyChange(PROP_EDITABLE, oldValue, editable);
+        }
+    }
+
+    @Inject
+    protected void setIcons(Icons icons) {
+        this.icons = icons;
+
+        setIcon(icons.get(CubaIcon.PICKERFIELD_CLEAR));
+    }
+
+    @Override
+    public void actionPerform(Component component) {
+        // if standard behaviour
+        if (!hasSubscriptions(ActionPerformedEvent.class)) {
+            execute();
+        } else {
+            // call action perform handlers from super, delegate execution
+            super.actionPerform(component);
+        }
+    }
+
+    /**
+     * Executes the action.
+     */
+    @SuppressWarnings("unchecked")
+    public void execute() {
+        // remove entity if it is a composition
+        Object value = pickerField.getValue();
+        ValueSource valueSource = pickerField.getValueSource();
+        if (value != null && !value.equals(pickerField.getEmptyValue()) && valueSource instanceof EntityValueSource) {
+            EntityValueSource entityValueSource = (EntityValueSource) pickerField.getValueSource();
+            Entity entity = (Entity) pickerField.getValue();
+            if (entityValueSource.getMetaPropertyPath() != null
+                    && entityValueSource.getMetaPropertyPath().getMetaProperty().getType() == MetaProperty.Type.COMPOSITION) {
+                FrameOwner screen = pickerField.getFrame().getFrameOwner();
+                DataContext dataContext = UiControllerUtils.getScreenData(screen).getDataContext();
+                dataContext.remove(entity);
+            }
+        }
+        // Set the value as if the user had set it
+        pickerField.setValueFromUser(pickerField.getEmptyValue());
+    }
+}
---
+++ jmix-ui/src/main/java/io/jmix/ui/actions/picker/OpenAction.java
@@ -0,0 +1,291 @@
+/*
+ * Copyright 2019 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.jmix.ui.actions.picker;
+
+import com.haulmont.chile.core.model.MetaClass;
+import com.haulmont.cuba.client.ClientConfig;
+import com.haulmont.cuba.core.entity.Entity;
+import com.haulmont.cuba.core.entity.SoftDelete;
+import com.haulmont.cuba.core.global.Configuration;
+import com.haulmont.cuba.core.global.DevelopmentException;
+import com.haulmont.cuba.core.global.Messages;
+import com.haulmont.cuba.gui.ComponentsHelper;
+import com.haulmont.cuba.gui.Notifications;
+import com.haulmont.cuba.gui.Notifications.NotificationType;
+import com.haulmont.cuba.gui.ScreenBuilders;
+import com.haulmont.cuba.gui.builders.EditorBuilder;
+import com.haulmont.cuba.gui.components.ActionType;
+import com.haulmont.cuba.gui.components.Component;
+import com.haulmont.cuba.gui.components.PickerField;
+import com.haulmont.cuba.gui.components.actions.BaseAction;
+import com.haulmont.cuba.gui.icons.CubaIcon;
+import com.haulmont.cuba.gui.icons.Icons;
+import com.haulmont.cuba.gui.meta.StudioAction;
+import com.haulmont.cuba.gui.meta.StudioDelegate;
+import com.haulmont.cuba.gui.meta.StudioPropertiesItem;
+import com.haulmont.cuba.gui.screen.*;
+import com.haulmont.cuba.gui.sys.ActionScreenInitializer;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.Nullable;
+import javax.inject.Inject;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+/**
+ * Standard picker field action for opening an entity instance in its editor screen.
+ * <p>
+ * Should be defined for {@code PickerField} or its subclass in a screen XML descriptor.
+ * <p>
+ * The action instance can be parameterized using the nested {@code properties} XML element or programmatically in the
+ * screen controller.
+ */
+@StudioAction(category = "Picker Actions", description = "Opens an entity using the entity edit screen")
+@ActionType(OpenAction.ID)
+public class OpenAction extends BaseAction implements PickerField.PickerFieldAction, InitializingBean {
+
+    public static final String ID = "picker_open";
+
+    protected PickerField<Entity> pickerField;
+    protected Icons icons;
+
+    protected Messages messages;
+    protected Configuration configuration;
+
+    @Inject
+    protected ScreenBuilders screenBuilders;
+
+    protected boolean editable = true;
+
+    protected ActionScreenInitializer screenInitializer = new ActionScreenInitializer();
+
+    public OpenAction() {
+        super(ID);
+    }
+
+    public OpenAction(String id) {
+        super(id);
+    }
+
+    /**
+     * Returns the editor screen open mode if it was set by {@link #setOpenMode(OpenMode)} or in the screen XML.
+     * Otherwise returns null.
+     */
+    @Nullable
+    public OpenMode getOpenMode() {
+        return screenInitializer.getOpenMode();
+    }
+
+    /**
+     * Sets the editor screen open mode.
+     */
+    @StudioPropertiesItem
+    public void setOpenMode(OpenMode openMode) {
+        screenInitializer.setOpenMode(openMode);
+    }
+
+    /**
+     * Returns the editor screen id if it was set by {@link #setScreenId(String)} or in the screen XML.
+     * Otherwise returns null.
+     */
+    @Nullable
+    public String getScreenId() {
+        return screenInitializer.getScreenId();
+    }
+
+    /**
+     * Sets the editor screen id.
+     */
+    @StudioPropertiesItem
+    public void setScreenId(String screenId) {
+        screenInitializer.setScreenId(screenId);
+    }
+
+    /**
+     * Returns the editor screen class if it was set by {@link #setScreenClass(Class)} or in the screen XML.
+     * Otherwise returns null.
+     */
+    @Nullable
+    public Class getScreenClass() {
+        return screenInitializer.getScreenClass();
+    }
+
+    /**
+     * Sets the editor screen id.
+     */
+    @StudioPropertiesItem
+    public void setScreenClass(Class screenClass) {
+        screenInitializer.setScreenClass(screenClass);
+    }
+
+    /**
+     * Sets the editor screen options supplier. The supplier provides {@code ScreenOptions} to the
+     * opened screen.
+     * <p>
+     * The preferred way to set the supplier is using a controller method annotated with {@link Install}, e.g.:
+     * <pre>
+     * &#64;Install(to = "petField.open", subject = "screenOptionsSupplier")
+     * protected ScreenOptions petFieldOpenScreenOptionsSupplier() {
+     *     return new MapScreenOptions(ParamsMap.of("someParameter", 10));
+     * }
+     * </pre>
+     */
+    @StudioDelegate
+    public void setScreenOptionsSupplier(Supplier<ScreenOptions> screenOptionsSupplier) {
+        screenInitializer.setScreenOptionsSupplier(screenOptionsSupplier);
+    }
+
+    /**
+     * Sets the editor screen configurer. Use the configurer if you need to provide parameters to the
+     * opened screen through setters.
+     * <p>
+     * The preferred way to set the configurer is using a controller method annotated with {@link Install}, e.g.:
+     * <pre>
+     * &#64;Install(to = "petField.open", subject = "screenConfigurer")
+     * protected void petFieldOpenScreenConfigurer(Screen editorScreen) {
+     *     ((PetEdit) editorScreen).setSomeParameter(someValue);
+     * }
+     * </pre>
+     */
+    @StudioDelegate
+    public void setScreenConfigurer(Consumer<Screen> screenConfigurer) {
+        screenInitializer.setScreenConfigurer(screenConfigurer);
+    }
+
+    /**
+     * Sets the handler to be invoked when the editor screen closes.
+     * <p>
+     * The preferred way to set the handler is using a controller method annotated with {@link Install}, e.g.:
+     * <pre>
+     * &#64;Install(to = "petField.open", subject = "afterCloseHandler")
+     * protected void petFieldOpenAfterCloseHandler(AfterCloseEvent event) {
+     *     CloseAction closeAction = event.getCloseAction();
+     *     System.out.println("Closed with " + closeAction);
+     * }
+     * </pre>
+     */
+    @StudioDelegate
+    public void setAfterCloseHandler(Consumer<Screen.AfterCloseEvent> afterCloseHandler) {
+        screenInitializer.setAfterCloseHandler(afterCloseHandler);
+    }
+
+    @Inject
+    protected void setConfiguration(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        setShortcut(configuration.getConfig(ClientConfig.class).getPickerOpenShortcut());
+        setDescription(messages.getMainMessage("pickerField.action.open.tooltip")
+                + " (" + getShortcutCombination().format() + ")");
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void setPickerField(PickerField pickerField) {
+        this.pickerField = pickerField;
+    }
+
+    @Override
+    public void editableChanged(PickerField pickerField, boolean editable) {
+        // open action is available in read-only picker
+        if (editable) {
+            setIcon(icons.get(CubaIcon.PICKERFIELD_OPEN));
+        } else {
+            setIcon(icons.get(CubaIcon.PICKERFIELD_OPEN_READONLY));
+        }
+    }
+
+    @Override
+    public boolean isEditable() {
+        return editable;
+    }
+
+    protected void setEditable(boolean editable) {
+        boolean oldValue = this.editable;
+        if (oldValue != editable) {
+            this.editable = editable;
+            firePropertyChange(PROP_EDITABLE, oldValue, editable);
+        }
+    }
+
+    @Inject
+    protected void setIcons(Icons icons) {
+        this.icons = icons;
+
+        setIcon(icons.get(CubaIcon.PICKERFIELD_OPEN));
+    }
+
+    @Inject
+    protected void setMessages(Messages messages) {
+        this.messages = messages;
+    }
+
+    @Override
+    public void actionPerform(Component component) {
+        // if standard behaviour
+        if (!hasSubscriptions(ActionPerformedEvent.class)) {
+            execute();
+        } else {
+            // call action perform handlers from super, delegate execution
+            super.actionPerform(component);
+        }
+    }
+
+    /**
+     * Executes the action.
+     */
+    public void execute() {
+        if (!checkFieldValue())
+            return;
+
+        Entity entity = pickerField.getValue();
+
+        if (entity instanceof SoftDelete && ((SoftDelete) entity).isDeleted()) {
+            ScreenContext screenContext = ComponentsHelper.getScreenContext(pickerField);
+            Notifications notifications = screenContext.getNotifications();
+
+            notifications.create(NotificationType.HUMANIZED)
+                    .withDescription(messages.getMainMessage("OpenAction.objectIsDeleted"))
+                    .show();
+
+            return;
+        }
+
+        MetaClass metaClass = pickerField.getMetaClass();
+        if (metaClass == null) {
+            throw new DevelopmentException("Neither metaClass nor datasource/property is specified " +
+                    "for the PickerField", "action ID", getId());
+        }
+
+        EditorBuilder builder = screenBuilders.editor(pickerField);
+
+        builder = screenInitializer.initBuilder(builder);
+
+        Screen editorScreen = builder.build();
+
+        screenInitializer.initScreen(editorScreen);
+
+        editorScreen.show();
+    }
+
+    protected boolean checkFieldValue() {
+        Entity entity = pickerField.getValue();
+        return entity != null;
+    }
+}
