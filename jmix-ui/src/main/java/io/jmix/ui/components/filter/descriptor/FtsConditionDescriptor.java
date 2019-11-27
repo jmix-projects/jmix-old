c060252342807d3b7200934f1d94dfa50a59eec3
Vlad Minaev <minaev@haulmont.com>
2019-11-27 11:50:04 +0400

    Copy filter from CUBA

---
+++ jmix-ui/src/main/java/io/jmix/ui/components/filter/descriptor/FtsConditionDescriptor.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2019 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.jmix.ui.components.filter.descriptor;
+
+import com.haulmont.chile.core.annotations.MetaClass;
+import com.haulmont.cuba.core.entity.annotation.SystemLevel;
+import com.haulmont.cuba.core.global.AppBeans;
+import com.haulmont.cuba.core.global.Messages;
+import com.haulmont.cuba.gui.components.filter.condition.AbstractCondition;
+import com.haulmont.cuba.gui.components.filter.condition.FtsCondition;
+
+@MetaClass(name = "sec$FtsConditionDescriptor")
+@SystemLevel
+public class FtsConditionDescriptor extends AbstractConditionDescriptor {
+
+    public FtsConditionDescriptor(String filterComponentName, com.haulmont.chile.core.model.MetaClass metaClass,
+                                  String entityAlias) {
+        super("fts", filterComponentName, metaClass, entityAlias);
+    }
+
+    @Override
+    public AbstractCondition createCondition() {
+        FtsCondition ftsCondition = new FtsCondition(this);
+        return ftsCondition;
+    }
+
+    @Override
+    public String getTreeCaption() {
+        Messages messages = AppBeans.get(Messages.NAME);
+        return messages.getMainMessage("filter.addCondition.ftsCondition");
+    }
+
+    @Override
+    public Class getJavaClass() {
+        return String.class;
+    }
+
+    @Override
+    public String getEntityParamWhere() {
+        return null;
+    }
+
+    @Override
+    public String getEntityParamView() {
+        return null;
+    }
+}
---
+++ jmix-ui/src/main/java/io/jmix/ui/components/filter/edit/PropertyConditionFrame.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2019 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.jmix.ui.components.filter.edit;
+
+import com.haulmont.cuba.gui.components.BoxLayout;
+import com.haulmont.cuba.gui.components.Component;
+import com.haulmont.cuba.gui.components.HasValue;
+import com.haulmont.cuba.gui.components.TextField;
+import com.haulmont.cuba.gui.components.filter.Param;
+import com.haulmont.cuba.gui.components.filter.condition.AbstractCondition;
+import com.haulmont.cuba.gui.components.filter.condition.PropertyCondition;
+
+import javax.inject.Inject;
+import java.util.Map;
+
+public class PropertyConditionFrame extends ConditionFrame<PropertyCondition> {
+
+    @Inject
+    protected BoxLayout operationLayout;
+
+    @Inject
+    protected TextField<String> caption;
+
+    @Inject
+    protected TextField property;
+
+    protected Component operationComponent;
+
+    @Override
+    public void init(Map<String, Object> params) {
+        super.init(params);
+    }
+
+    @Override
+    public void setCondition(PropertyCondition condition) {
+        super.setCondition(condition);
+        if (operationComponent != null)
+            operationLayout.remove(operationComponent);
+        operationComponent = condition.createOperationEditor().getComponent();
+        operationLayout.add(operationComponent);
+        caption.setValue(condition.getCaption());
+        property.setValue(condition.getPropertyLocCaption());
+
+        condition.addListener(new AbstractCondition.Listener() {
+            @Override
+            public void captionChanged() {}
+
+            @Override
+            public void paramChanged(Param oldParam, Param newParam) {
+                Component oldDefaultValueComponent = defaultValueComponent;
+                createDefaultValueComponent();
+                if (defaultValueComponent != null && defaultValueComponent instanceof HasValue
+                        && oldDefaultValueComponent != null && oldDefaultValueComponent instanceof HasValue) {
+                    if (oldParam.getJavaClass().equals(newParam.getJavaClass())
+                            && defaultValueComponent.getClass().equals(oldDefaultValueComponent.getClass())) {
+                        ((HasValue) defaultValueComponent).setValue(((HasValue) oldDefaultValueComponent).getValue());
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean commit() {
+        if (!super.commit())
+            return false;
+        condition.setCaption(caption.getValue());
+        return true;
+    }
+}
