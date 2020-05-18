757d0ee4c9237f136fac71e6c6615119f0307850
Roman Pinyazhin <pinyazhin@haulmont.com>
2020-05-18 11:39:56 +0400

    Collapse mode in side menu #196

--- samples/sample-ui/src/main/java/io/jmix/samples/ui/screen/MainScreen.java
+++ samples/sample-ui/src/main/java/io/jmix/samples/ui/screen/MainScreen.java
@@ -16,18 +16,20 @@
 
 package io.jmix.samples.ui.screen;
 
-import io.jmix.core.Metadata;
-import io.jmix.ui.Notifications;
-import io.jmix.ui.ScreenBuilders;
-import io.jmix.ui.UiComponents;
-import io.jmix.ui.components.AppWorkArea;
-import io.jmix.ui.components.Window;
-import io.jmix.ui.screen.Screen;
-import io.jmix.ui.screen.UiController;
-import io.jmix.ui.screen.UiDescriptor;
+import com.vaadin.server.WebBrowser;
+import io.jmix.core.Messages;
+import io.jmix.ui.*;
+import io.jmix.ui.components.*;
+import io.jmix.ui.components.dev.LayoutAnalyzerContextMenuProvider;
+import io.jmix.ui.components.mainwindow.AppMenu;
+import io.jmix.ui.components.mainwindow.SideMenu;
+import io.jmix.ui.components.mainwindow.UserIndicator;
+import io.jmix.ui.screen.*;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.annotation.Nullable;
-import javax.inject.Inject;
+
+import static io.jmix.ui.components.ComponentsHelper.setStyleName;
 
 /**
  * Base class for a controller of application Main screen.
@@ -36,18 +38,233 @@ import javax.inject.Inject;
 @UiController("main")
 public class MainScreen extends Screen implements Window.HasWorkArea {
 
-    @Inject
-    protected Notifications notifications;
-    @Inject
-    private Metadata metadata;
-    @Inject
-    private UiComponents uiComponents;
-    @Inject
-    private ScreenBuilders screenBuilders;
+    public static final String SIDEMENU_COLLAPSED_STATE = "sidemenuCollapsed";
+    public static final String SIDEMENU_COLLAPSED_STYLENAME = "collapsed";
+
+    protected static final String APP_LOGO_IMAGE = "application.logoImage";
+
+    public MainScreen() {
+        addInitListener(this::initComponents);
+    }
+
+    protected void initComponents(@SuppressWarnings("unused") InitEvent e) {
+        initLogoImage();
+        initFtsField();
+        initUserIndicator();
+//        initTitleBar();
+        initMenu();
+        initLayoutAnalyzerContextMenu();
+    }
+
+    protected void initUserIndicator() {
+        UserIndicator userIndicator = getUserIndicator();
+        if (userIndicator != null) {
+            boolean authenticated = AppUI.getCurrent().hasAuthenticatedSession();
+            userIndicator.setVisible(authenticated);
+        }
+    }
+
+    protected void initLogoImage() {
+        Image logoImage = getLogoImage();
+        String logoImagePath = getBeanLocator().get(Messages.class)
+                .getMessage(APP_LOGO_IMAGE);
+
+        if (logoImage != null
+                && StringUtils.isNotBlank(logoImagePath)
+                && !APP_LOGO_IMAGE.equals(logoImagePath)) {
+            logoImage.setSource(ThemeResource.class).setPath(logoImagePath);
+        }
+    }
+
+    protected void initFtsField() {
+        // TODO fts
+        /*FtsField ftsField = getFtsField();
+        if (ftsField != null && !FtsConfigHelper.getEnabled()) {
+            ftsField.setVisible(false);
+        }*/
+    }
+
+    protected void initLayoutAnalyzerContextMenu() {
+        Image logoImage = getLogoImage();
+        if (logoImage != null) {
+            LayoutAnalyzerContextMenuProvider laContextMenuProvider =
+                    getBeanLocator().get(LayoutAnalyzerContextMenuProvider.NAME);
+            laContextMenuProvider.initContextMenu(this, logoImage);
+        }
+    }
+
+    protected void initMenu() {
+        Component menu = getAppMenu();
+        if (menu == null) {
+            menu = getSideMenu();
+        }
+
+        if (menu != null) {
+            ((Component.Focusable) menu).focus();
+        }
+
+        initCollapsibleMenu();
+    }
+
+    protected void initCollapsibleMenu() {
+        Component sideMenuContainer = getWindow().getComponent("sideMenuContainer");
+        if (sideMenuContainer instanceof CssLayout) {
+            if (isMobileDevice()) {
+                setSideMenuCollapsed(true);
+            } else {
+                String menuCollapsedCookie = App.getInstance()
+                        .getCookieValue(SIDEMENU_COLLAPSED_STATE);
+
+                boolean menuCollapsed = Boolean.parseBoolean(menuCollapsedCookie);
+
+                setSideMenuCollapsed(menuCollapsed);
+            }
+
+            initCollapseMenuControls();
+        }
+    }
+
+    protected void initCollapseMenuControls() {
+        Button collapseMenuButton = getCollapseMenuButton();
+        if (collapseMenuButton != null) {
+            collapseMenuButton.addClickListener(event ->
+                    setSideMenuCollapsed(!isMenuCollapsed()));
+        }
+
+        Button settingsButton = getSettingsButton();
+        if (settingsButton != null) {
+            settingsButton.addClickListener(event ->
+                    openSettingsScreen());
+        }
+
+        Button loginButton = getLoginButton();
+        if (loginButton != null) {
+            loginButton.addClickListener(event ->
+                    openLoginScreen());
+        }
+    }
+
+//    protected void initTitleBar() {
+//        if (getBeanLocator().get(UiProperties.class).isUseInverseHeader()) {
+//            Component titleBar = getTitleBar();
+//            if (titleBar != null) {
+//                titleBar.setStyleName("c-app-menubar c-inverse-header");
+//            }
+//        }
+//    }
+
+    @Subscribe
+    protected void onAfterShow(AfterShowEvent event) {
+        Screens screens = UiControllerUtils.getScreenContext(this)
+                .getScreens();
+        getBeanLocator().get(ScreenTools.class)
+                .openDefaultScreen(screens);
+    }
 
     @Nullable
     @Override
     public AppWorkArea getWorkArea() {
         return (AppWorkArea) getWindow().getComponent("workArea");
+    }
+
+    @Nullable
+    public UserIndicator getUserIndicator() {
+        return (UserIndicator) getWindow().getComponent("userIndicator");
+    }
+
+    @Nullable
+    protected Button getCollapseMenuButton() {
+        return (Button) getWindow().getComponent("collapseMenuButton");
+    }
+
+    @Nullable
+    protected Button getSettingsButton() {
+        return (Button) getWindow().getComponent("settingsButton");
+    }
+
+    @Nullable
+    protected Button getLoginButton() {
+        return (Button) getWindow().getComponent("loginButton");
+    }
+
+    @Nullable
+    protected Image getLogoImage() {
+        return (Image) getWindow().getComponent("logoImage");
+    }
+
+    // TODO fts
+    /*@Nullable
+    protected FtsField getFtsField() {
+        return (FtsField) getWindow().getComponent("ftsField");
+    }*/
+
+    @Nullable
+    protected AppMenu getAppMenu() {
+        return (AppMenu) getWindow().getComponent("appMenu");
+    }
+
+    @Nullable
+    protected SideMenu getSideMenu() {
+        return (SideMenu) getWindow().getComponent("sideMenu");
+    }
+
+    @Nullable
+    protected Component getTitleBar() {
+        return getWindow().getComponent("titleBar");
+    }
+
+    protected void openLoginScreen() {
+        String loginScreenId = getBeanLocator().get(UiProperties.class).getLoginScreenId();
+
+        UiControllerUtils.getScreenContext(this)
+                .getScreens()
+                .create(loginScreenId, OpenMode.ROOT)
+                .show();
+    }
+
+    protected void openSettingsScreen() {
+        UiControllerUtils.getScreenContext(this)
+                .getScreens()
+                .create("settings", OpenMode.NEW_TAB)
+                .show();
+    }
+
+    protected void setSideMenuCollapsed(boolean collapsed) {
+        Component sideMenuContainer = getWindow().getComponent("sideMenuContainer");
+        CssLayout sideMenuPanel = (CssLayout) getWindow().getComponent("sideMenuPanel");
+        Button collapseMenuButton = getCollapseMenuButton();
+
+        setStyleName(sideMenuContainer, SIDEMENU_COLLAPSED_STYLENAME, collapsed);
+        setStyleName(sideMenuPanel, SIDEMENU_COLLAPSED_STYLENAME, collapsed);
+
+        if (collapseMenuButton != null) {
+            Messages messages = getBeanLocator().get(Messages.class);
+            if (collapsed) {
+                collapseMenuButton.setCaption(messages.getMessage("menuExpandGlyph"));
+                collapseMenuButton.setDescription(messages.getMessage("sideMenuExpand"));
+            } else {
+                collapseMenuButton.setCaption(messages.getMessage("menuCollapseGlyph"));
+                collapseMenuButton.setDescription(messages.getMessage("sideMenuCollapse"));
+            }
+        }
+
+        App.getInstance()
+                .addCookie(SIDEMENU_COLLAPSED_STATE, String.valueOf(collapsed));
+    }
+
+    protected boolean isMenuCollapsed() {
+        CssLayout sideMenuPanel = (CssLayout) getWindow().getComponent("sideMenuPanel");
+        return sideMenuPanel != null
+                && sideMenuPanel.getStyleName() != null
+                && sideMenuPanel.getStyleName().contains(SIDEMENU_COLLAPSED_STYLENAME);
+    }
+
+    protected boolean isMobileDevice() {
+        WebBrowser browser = AppUI.getCurrent()
+                .getPage()
+                .getWebBrowser();
+
+        return browser.getScreenWidth() < 500
+                || browser.getScreenHeight() < 800;
     }
 }
