7cb566a68b4cf76ed8d9f1f9c97e4951ac6535ce
Konstantin Krivopustov <krivopustov@haulmont.com>
2020-06-24 09:26:39 +0400

    Remove generation of unused method #512

--- jmix-gradle-plugin/src/main/java/io/jmix/gradle/EntityEntryEnhancingStep.java
+++ jmix-gradle-plugin/src/main/java/io/jmix/gradle/EntityEntryEnhancingStep.java
@@ -112,15 +112,6 @@ public class EntityEntryEnhancingStep extends BaseEnhancingStep {
                                 StringUtils.capitalize(generatedIdField.getName())),
                         nestedCtClass);
                 nestedCtClass.addMethod(getGeneratedIdMethod);
-
-                CtMethod setGeneratedIdMethod = CtNewMethod.make(CtClass.voidType, "setGeneratedId",
-                        new CtClass[]{idType}, null,
-                        String.format("((%s)getSource()).set%s((%s)$1);",
-                                ctClass.getName(),
-                                StringUtils.capitalize(generatedIdField.getName()),
-                                generatedIdField.getType().getName()),
-                        nestedCtClass);
-                nestedCtClass.addMethod(setGeneratedIdMethod);
             }
         }
 
