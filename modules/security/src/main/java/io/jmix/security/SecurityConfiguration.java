9a05cdb474779c57a119ad041a4e0cef7878aaa6
subbotin <subbotin@haulmont.com>
2020-08-04 12:51:53 +0400

    Security authorization API and core implementation #445

--- modules/security/src/main/java/io/jmix/security/SecurityConfiguration.java
+++ modules/security/src/main/java/io/jmix/security/SecurityConfiguration.java
@@ -18,6 +18,7 @@ package io.jmix.security;
 
 import io.jmix.core.CoreConfiguration;
 import io.jmix.core.annotation.JmixModule;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
--- samples/sample-custom-security/src/main/java/io/jmix/samples/customsecurity/SampleCustomSecurityApplication.java
+++ samples/sample-custom-security/src/main/java/io/jmix/samples/customsecurity/SampleCustomSecurityApplication.java
@@ -1,20 +1,15 @@
 package io.jmix.samples.customsecurity;
 
-import io.jmix.core.security.Security;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import javax.sql.DataSource;
 
 @SpringBootApplication
 public class SampleCustomSecurityApplication implements CommandLineRunner {
-
-    @Autowired
-    private Security security;
 
     public static void main(String[] args) {
         SpringApplication.run(SampleCustomSecurityApplication.class, args);
@@ -22,7 +17,7 @@ public class SampleCustomSecurityApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Security implementation is " + security.getClass().getName());
+
     }
 
     @Bean
